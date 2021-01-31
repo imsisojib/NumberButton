7d64621e832448a1aaf10b9b010f6907f715b93c
imsisojib <sirajulislam1123@gmail.com>
2021-01-31 13:15:55 +0600

    module implemented and tested.

--- app/src/main/java/com/imsisojib/customnumberbutton/MainActivity.java
+++ app/src/main/java/com/imsisojib/customnumberbutton/MainActivity.java
@@ -3,12 +3,24 @@ package com.imsisojib.customnumberbutton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.imsisojib.number_button.NumberButton;
 
 public class MainActivity extends AppCompatActivity {
+    private NumberButton numberButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        numberButton = findViewById(R.id.number_button);
+
+        findViewById(R.id.button_save).setOnClickListener(v -> {
+            int number = numberButton.getNumber();
+            Toast.makeText(this, "Number = "+number, Toast.LENGTH_SHORT).show();
+        });
+
     }
 }
